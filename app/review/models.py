ac6ca3929e901b3e56366885861a0e8a5a3b1440
root <root@yooooonheera>
2025-07-30 13:34:37 +0900

    modified urls.py,models.py,views.py add serialisers.py, permissions.py

--- app/review/models.py
+++ app/review/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from book.models import Book
+from user.models import User
+from django.core.validators import MinValueValidator, MaxValueValidator
 
-# Create your models here.
+class Review(models.Model):
+    review_id = models.AutoField(primary_key=True)  
+    book_id = models.ForeignKey(Book, on_delete=models.CASCADE)   
+    user_id = models.ForeignKey(User, on_delete=models.CASCADE)   
+    rating = models.IntegerField(validators=[MinValueValidator(1), MaxValueValidator(5)],default=1)
+    content = models.CharField(max_length=200)
+    created_at = models.DateTimeField(auto_now_add=True) 
+    updated_at = models.DateTimeField(auto_now=True)     
+    def __str__(self):
+        return f"{self.user_id} rated {self.book_id} {self.rating}"
---
+++ app/review/permissions.py
@@ -0,0 +1,9 @@
+from rest_framework import permissions
+
+class IsOwnerOrReadOnly(permissions.BasePermission):
+    def has_object_permission(self, request, view, obj):
+        # 로그인 안해도 읽기 가능
+        if request.method in permissions.SAFE_METHODS:
+            return True
+        # 로그인해야 읽기 가능
+        return obj.user_id == request.user 
