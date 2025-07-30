d7eca25d98d90070a67ac2ed835dcce9095211b7
root <root@DESKTOP-ARVSK27>
2025-07-30 13:42:08 +0900

    Add Book model with migrations

--- app/book/models.py
+++ app/book/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+class Author(models.Model):
+      name = models.CharField(max_length=20)
+      
+      def __str__(self):
+            return self.name
+
+
+class Books(models.Model):
+      title = models.CharField(max_length=100)
+      author = models.ForeignKey(Author,on_delete=models.CASCADE, related_name='books')
+      published_at = models.DateTimeField(auto_now_add=True)
+      
+      def __str__(self):
+            return self.title
--- app/book/urls.py
+++ app/book/urls.py
@@ -2,4 +2,5 @@ from django.urls import path
 from . import views  # 혹은 from book import views
 
 urlpatterns = [
+      path("", views.books, name="books"),
 ]
