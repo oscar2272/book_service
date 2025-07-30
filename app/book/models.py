5f44d59396deea4d25a455d8ec69b858f6501d0c
root <root@DESKTOP-ARVSK27>
2025-07-30 14:47:04 +0900

    Add Book model with migrations

--- app/book/models.py
+++ app/book/models.py
@@ -7,7 +7,7 @@ class Author(models.Model):
             return self.name
 
 
-class Books(models.Model):
+class Book(models.Model):
       title = models.CharField(max_length=100)
       author = models.ForeignKey(Author,on_delete=models.CASCADE, related_name='books')
       published_at = models.DateTimeField(auto_now_add=True)
